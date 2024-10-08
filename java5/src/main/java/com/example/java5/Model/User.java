2758b9a114649aef419f2b3f0b3010379c8a4eec
Duyentr2024 <156353407+Duyentr2024@users.noreply.github.com>
2024-10-08 23:51:29 +0700

    create project

---
+++ java5/src/main/java/com/example/java5/Model/User.java
@@ -0,0 +1,16 @@
+package com.example.java5.Model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String email;
+    private String password;
+    private String fullName;
+}
